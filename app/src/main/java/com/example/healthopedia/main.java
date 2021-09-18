10315382a3bdffebeaabda2169192b7d90ee4dd1
kethmi <it20021320@my.sliit.lk>
2021-09-18 22:16:09 +0530

    add file

--- app/src/main/java/com/example/healthopedia/main.java
+++ app/src/main/java/com/example/healthopedia/main.java
@@ -6,18 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class main extends AppCompatActivity {
-    Button btn_vhis;
+    private Button btn_vhis;
+    private Button btn_ok;
+    private TextView tv_ans;
+    private EditText Etheight;
+    private EditText Etweight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        btn_vhis = (Button)findViewById(R.id.btn_vhis);
 
-        btn_vhis = findViewById(R.id.btn_vhis);
+        btn_ok = (Button)findViewById(R.id.btn_ok);
+        tv_ans = (TextView) findViewById(R.id.tv_ans);
+        Etheight = (EditText)findViewById(R.id.Etheight);
+        Etweight = (EditText)findViewById(R.id.Etweight);
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float num2 = Integer.parseInt(Etheight.getText().toString());
+                float num3 = Integer.parseInt(Etweight.getText().toString());
+                float s = (num2 * num2);
+                float t = num3/s;
+                tv_ans.setText("ans : "+t);
+            }
+        });
+
     }
 
+    public void sum(View v)
+    {
+
+    }
     public void OnClick14(View v){
         Intent intent = new Intent(this,main6.class);
         startActivity(intent);
