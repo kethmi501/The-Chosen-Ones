e1d30b294335b32c8bc506be970ff08a71bb9919
kethmi <it20021320@my.sliit.lk>
2021-09-26 16:14:19 +0530

    change file

--- app/src/main/java/com/example/healthopedia/database/DBhelper.java
+++ app/src/main/java/com/example/healthopedia/database/DBhelper.java
@@ -7,97 +7,85 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.view.View;
 
-import androidx.annotation.Nullable;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class DBhelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "bmiCalc.db";
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "food.db";
 
-
-    public DBhelper( Context context) { super(context, DATABASE_NAME, null, 1); }
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String SQL_CREATE_ENTRIES =
-                "CREATE TABLE " + UserM.Users.TABLE_NAME + " ( " +
-                        UserM.Users._ID+ " INTEGER_PRIMARY_KEY," +
-                        UserM.Users.COLUMN_NAME_UNAME+ " TEXT," +
-                        UserM.Users.COLUMN_NAME_HEIGHT+ " INTEGER," +
-                        UserM.Users.COLUMN_NAME_WEIGHT+ " INTEGER,"+
-                        UserM.Users.COLUMN_NAME_ANSWER+ " INTEGER)";
+                "CREATE TABLE " + FoodManage.Food.TABLE_NAME + " (" +
+                        FoodManage.Food._ID + " INTEGER PRIMARY KEY," +
+                        FoodManage.Food.COLUMN_NAME_FOODNAME + " TEXT,"+
+                        FoodManage.Food.COLUMN_NAME_SIZE + " TEXT,"+
+                        FoodManage.Food.COLUMN_NAME_CALORY + " TEXT)";
 
-        db.execSQL(SQL_CREATE_ENTRIES);
+        sqLiteDatabase.execSQL(SQL_CREATE_ENTRIES);
+
     }
-
-    public Long addInfo(String name, String height, String weight, String bmi){
+    public long addFood(String foodName, String size, String calory){
         SQLiteDatabase db = getWritableDatabase();
-
         ContentValues values = new ContentValues();
-        values.put(UserM.Users.COLUMN_NAME_UNAME, name);
-        values.put(UserM.Users.COLUMN_NAME_HEIGHT, height);
-        values.put(UserM.Users.COLUMN_NAME_WEIGHT, weight);
-        values.put(UserM.Users.COLUMN_NAME_ANSWER, bmi);
+        values.put(FoodManage.Food.COLUMN_NAME_FOODNAME,foodName);
+        values.put(FoodManage.Food.COLUMN_NAME_SIZE,size);
+        values.put(FoodManage.Food.COLUMN_NAME_CALORY,calory);
 
-        return  db.insert(UserM.Users.TABLE_NAME, null, values);
+        return db.insert(FoodManage.Food.TABLE_NAME,null,values);
     }
 
-
-    public List readAll(){
+    public List readAllFoods(){
         SQLiteDatabase db = getReadableDatabase();
-        String [] projection = {
-                UserM.Users._ID,
-                UserM.Users.COLUMN_NAME_UNAME,
-                UserM.Users.COLUMN_NAME_HEIGHT,
-                UserM.Users.COLUMN_NAME_WEIGHT,
-                UserM.Users.COLUMN_NAME_ANSWER
+        String[] projection ={
+                FoodManage.Food._ID,
+                FoodManage.Food.COLUMN_NAME_FOODNAME,
+                FoodManage.Food.COLUMN_NAME_SIZE,
+                FoodManage.Food.COLUMN_NAME_CALORY
         };
-
-        String sortOrder = UserM.Users._ID + " DESC";
-
         Cursor cursor = db.query(
-                UserM.Users.TABLE_NAME,
-                projection,
-                null,
-                null,
-                null,
-                null,
-                sortOrder
-
+                FoodManage.Food.TABLE_NAME,
+                projection,null,null,null,null,null
         );
-
         List info = new ArrayList<>();
-
-        while(cursor.moveToNext()){
-            String name = cursor.getString(cursor.getColumnIndexOrThrow(UserM.Users.COLUMN_NAME_UNAME));
-            String height = cursor.getString(cursor.getColumnIndexOrThrow(UserM.Users.COLUMN_NAME_HEIGHT));
-            String weight = cursor.getString(cursor.getColumnIndexOrThrow(UserM.Users.COLUMN_NAME_WEIGHT));
-            String bmi = cursor.getString(cursor.getColumnIndexOrThrow(UserM.Users.COLUMN_NAME_ANSWER));
-
-            //info.add(name+": "+height+"cm: "+weight+"kg: "+bmi);
-            info.add(name+" : \n"+height+"cm \n"+weight+"kg \n"+bmi+"\n");
+        while (cursor.moveToNext()){
+            String foodName = cursor.getString(cursor.getColumnIndexOrThrow(FoodManage.Food.COLUMN_NAME_FOODNAME));
+            String size = cursor.getString(cursor.getColumnIndexOrThrow(FoodManage.Food.COLUMN_NAME_SIZE));
+            String calory = cursor.getString(cursor.getColumnIndexOrThrow(FoodManage.Food.COLUMN_NAME_CALORY));
+            info.add("Name : "+foodName+"\nSize : "+size+"\nCalory : "+calory+"\n\n");
         }
         cursor.close();
         return info;
     }
 
-    //delete method
-    public void deleteInfo(String height){
+    public void deleteFood(String foodName){
         SQLiteDatabase db = getReadableDatabase();
+        String selection = FoodManage.Food.COLUMN_NAME_FOODNAME + " LIKE ?";
+        String[] stringArgs = {foodName};
 
-        String selections = UserM.Users.COLUMN_NAME_UNAME + " LIKE ?";
-        String[] stringargs = {height};
-
-        db.delete(UserM.Users.TABLE_NAME,selections,stringargs);
+        db.delete(FoodManage.Food.TABLE_NAME,selection,stringArgs);
     }
 
+    public void updateInfo(View view, String foodName, String size, String calory){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(FoodManage.Food.COLUMN_NAME_SIZE,size);
+        values.put(FoodManage.Food.COLUMN_NAME_CALORY,calory);
 
+        String selection = FoodManage.Food.COLUMN_NAME_FOODNAME + " LIKE ?";
+        String[] selectionArgs = {foodName};
+
+        int count = db.update(
+                FoodManage.Food.TABLE_NAME,values,selection,selectionArgs
+        );
+    }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
 }
